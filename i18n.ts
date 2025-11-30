669b2b70f7af02d7b018cd00ecb93218f5c752d6
tranthanhduysph-coder <tranthanhduy.sph@gmail.com>
2025-11-30 13:26:01 +0700

    Initialize i18n with Vietnamese and English support

---
+++ i18n.ts
@@ -0,0 +1,21 @@
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { vi } from './locales/vi';
+import { en } from './locales/en';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: en,
+      vi: vi
+    },
+    lng: "vi", // Default language
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
