d8885582c4a4b5968b4af0f8986853fa4296d054
tranthanhduysph-coder <tranthanhduy.sph@gmail.com>
2025-11-30 16:14:33 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        sky: {
+          400: '#38bdf8',
+          500: '#0ea5e9',
+          600: '#0284c7',
+        }
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+      }
+    },
+  },
+  plugins: [],
+}
