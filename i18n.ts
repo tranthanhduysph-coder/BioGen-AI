9e5961b697b3d75a30be49b3094633a85f424abf
tranthanhduysph-coder <tranthanhduy.sph@gmail.com>
2025-11-30 14:38:03 +0700

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -3,22 +3,20 @@ import { initReactI18next } from 'react-i18next';
 import { vi } from './locales/vi';
 import { en } from './locales/en';
 
-// Khởi tạo i18n
 i18n
-  .use(initReactI18next) // Kết nối với React
+  .use(initReactI18next)
   .init({
     resources: {
-      en: en, // Nguồn ngôn ngữ Anh
-      vi: vi  // Nguồn ngôn ngữ Việt
+      en: en,
+      vi: vi
     },
-    lng: "vi", // Ngôn ngữ mặc định ban đầu là Tiếng Việt
-    fallbackLng: "en", // Nếu không tìm thấy key tiếng Việt thì dùng tiếng Anh
-    debug: true, // Bật debug để xem lỗi trong Console nếu có
+    lng: "vi", // Mặc định tiếng Việt
+    fallbackLng: "en",
     interpolation: {
-      escapeValue: false // React đã tự động xử lý XSS nên không cần escape
+      escapeValue: false
     },
     react: {
-        useSuspense: false // Tắt Suspense để tránh lỗi loading trắng trang
+      useSuspense: false
     }
   });
 
